2fb097e36aa2faebba2ef1ad405e400a4eaecedd
Shakvilla <abdulshekurabdullahclement@gmail.com>
2020-07-02 21:46:35 +0000

    first commit

---
+++ superglobal.php
@@ -0,0 +1,17 @@
+<?php
+// Variables that can be used anywhere in the code without the global keyword
+
+
+function getPostValue($key, $default = Null)
+{
+
+
+    if (isset($_POST[$key])) {
+        return $_POST[$key];
+    }
+
+    return $default;
+}
+
+
+echo getPostValue('email');
